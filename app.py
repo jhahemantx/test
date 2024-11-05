bdd36442dc93b5bfa7eea4c66d129b7d12add7c6
Hemant Jha <jhahemantx@gmail.com>
2024-11-05 16:26:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,6 @@ from langchain_core.output_parsers import StrOutputParser
 load_dotenv()
 
 ## Langsmith Tracking
-os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
-os.environ["LANGCHAIN_TRACING_V2"]="true"
-os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
 
 ## Prompt Template
 prompt=ChatPromptTemplate.from_messages(
